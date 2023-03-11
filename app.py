9002c1a00f39cb6792d0854571c8fd6c85c1f245
sahay-shashank <sahayshashank28@gmail.com>
2023-03-11 17:00:01 +0530

    get graph and information about the company

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # Yahoo Finance is used to gather real-time and historical stocks data from Yahoo
-import yfinance as yf
+import yahooquery as yf
 # matplotlib is used to plot the data in a graph
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+import numpy as np
 import json
 from flask import Flask, request , render_template, redirect
 app = Flask(__name__)
@@ -11,24 +12,26 @@ app = Flask(__name__)
 def rendering():
     return render_template('plot.html')
     
-@app.route('/plotgraph',methods=['POST'])
+@app.route('/plotgraph',methods=['POST']) # type: ignore
 def plot():
     result = request.get_json()
     print(result)
     # takes ticker to get stock data
-    t = yf.Ticker(result['ticker'])
+    company = yf.Ticker(result['ticker'],country="India")
     # gets the history for the ticker
-    thist = t.history(result['period'], result['interval'])
-    items = thist[result['option']]
-    fig = go.Figure(data=[go.Scatter(x=items.index, y=items.values)], layout=go.Layout(
+    stock_history = company.history(result['period'], result['interval'])
+    if(stock_history.empty):
+        print("Not available")
+        return {'result': 0}
+    values_of_option = stock_history[result['option'].lower()]
+    fig = go.Figure(data=[go.Scatter(x=values_of_option.index.get_level_values("date"), y=values_of_option.values)], layout=go.Layout(
         xaxis=go.layout.XAxis(showline=True, showspikes=True, spikedash="solid",
                             spikemode="toaxis+across", spikethickness=1, spikecolor="red")
     ))
     # plots in a graph
-    # fig.write_html("plot_frame.html")
     plot_json = fig.to_json()
-    # print(plot_json)
-    print('Done')
-    return plot_json
+    return {'profile': json.dumps(company.asset_profile),'result_plot':plot_json}
+    # return plot_json
+
 if __name__=='__main__':
     app.run(debug=True)
--- stock.py
+++ stock.py
@@ -1,5 +1,5 @@
 # Yahoo Finance is used to gather real-time and historical stocks data from Yahoo
-import yfinance as yf
+import yahooquery as yf
 # matplotlib is used to plot the data in a graph
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
@@ -9,10 +9,11 @@ t = yf.Ticker('SBIN.NS')
 print(t)
 # gets the history for the ticker
 thist = t.history("5y", "1d")
-items = thist["Close"]
-fig = go.Figure(data=[go.Scatter(x=items.index, y=items.values)], layout=go.Layout(
+items = thist["close"]
+fig = go.Figure(data=[go.Scatter(x=items.index.get_level_values("date"), y=items.values)], layout=go.Layout(
     xaxis=go.layout.XAxis(showline=True, showspikes=True, spikedash="solid",
                           spikemode="toaxis+across", spikethickness=1, spikecolor="red")
 ))
 # plots in a graph
-fig.write_html("plot.html")
+# fig.write_html("plot.html")
+fig.show()
